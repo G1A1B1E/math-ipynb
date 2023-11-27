d8658f815ac810113e5bfdd77094007e5b28466e
Gabe Macri <gabemacru@gmail.com>
2023-11-27 16:33:29 +0000

    save

---
+++ master.py
@@ -0,0 +1,58 @@
+def func()
+    
+
+    terms = 10
+
+# Uncomment code below to take input from the user
+# terms = int(input("How many terms? "))
+
+# use anonymous function
+    result = list(map(lambda x: 2 ** x, range(terms)))
+
+    print("The total terms are:",terms)
+    for i in range(terms):
+   print("2 raised to power",i,"is",result[i])
+
+
+def func1()
+   # Python program to find the largest number among the three input numbers
+
+# change the values of num1, num2 and num3
+# for a different result
+num1 = 10
+num2 = 14
+num3 = 12
+
+# uncomment following lines to take three numbers from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("The largest number is", largest)
+
+def func2()
+   # Python program to display the Fibonacci sequence
+
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
+
